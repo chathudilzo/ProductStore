8cd22b56104bf7d2e34f5e522fc8e6f2d47acf88
chathudilzo <chathuradilshanudawaththa@gmail.com>
2024-03-25 04:30:07 +0530

    Add project files.

---
+++ ProductStore/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace ProductStore
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ ProductStore/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace ProductStore
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
